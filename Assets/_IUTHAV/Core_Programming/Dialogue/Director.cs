78955d4ec8fdf621557691a5760ecabf6ec3e8f7
Martin S <mart.siedler@gmail.com>
2024-04-09 11:04:32 +0200

    Started ComicLineView

---
+++ Assets/_IUTHAV/Core_Programming/Dialogue/Director.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Yarn.Unity;
+using Yarn.Unity.Example;
+
+namespace _IUTHAV.Core_Programming.Dialogue {
+
+
+    public class Director : MonoBehaviour {
+
+        [SerializeField] private ComicBubbleView lineView;
+        [SerializeField] private GameObject continueButton;
+        [SerializeField] private CharacterBox[] characterBoxes;
+
+        private Dictionary<string, CharacterBox> _mCharacterBoxes;
+
+#region Unity Functions
+
+        private void Awake() {
+            Configure();
+        }
+
+#endregion
+
+#region Public Functions
+
+        
+
+#endregion
+
+#region Private Functions
+
+        private void Configure() {
+
+            _mCharacterBoxes = new Dictionary<string, CharacterBox>();
+            foreach (CharacterBox box in characterBoxes) {
+                _mCharacterBoxes.Add(box.name, box);
+            }
+
+        }
+
+        private IEnumerator WaitAndContinue(int waitTime) {
+
+            yield return new WaitForSeconds(waitTime);
+            lineView.OnContinueClicked();
+
+        }
+
+#endregion
+
+    }
+}
