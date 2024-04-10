c865c6873496d30dbb1ccd33c54ec5bef9be6b12
Martin Laptop <mart.siedler@gmail.com>
2024-04-10 09:32:56 +0200

    Update ComicLineView

---
+++ Assets/_IUTHAV/Core_Programming/Dialogue/TempPanelParameters.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+namespace _IUTHAV.Core_Programming.Dialogue {
+    [Serializable]
+    public class TempPanelParameters : MonoBehaviour {
+
+        public string[] panelCharacters;
+
+    }
+}
