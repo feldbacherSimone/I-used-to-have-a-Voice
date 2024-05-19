8853d4ec7a76e7c8410c41e5bc319ccd1dc3ea8c
Martin S <mart.siedler@gmail.com>
2024-05-19 21:21:36 +0200

    Preparing Prototype build 0.5

--- Assets/_IUTHAV/Scripts/Core/Gamemode/GameState.cs
+++ Assets/_IUTHAV/Scripts/Core/Gamemode/GameState.cs
@@ -5,60 +5,6 @@ using UnityEngine.Events;
 using UnityEngine.Serialization;
 
 namespace _IUTHAV.Scripts.Core.Gamemode {
-
-#region ScriptableObject Implementation
-
-    [CreateAssetMenu(fileName = "GameStatesObject", menuName = "ScriptableObjects/GameStatesObject", order = 2)]
-    public class GameStatesObject : ScriptableObject {
-        [Space(10)]
-        [SerializeField] private bool resetStatesOnUnload = true;
-        [FormerlySerializedAs("prefix")] [FormerlySerializedAs("dataType")] [SerializeField] private StatePrefix statePrefix;
-        public StatePrefix StatePrefix => statePrefix;
-        [Space(10)]
-        [SerializeField] private List<GameState> gameStates;
-        [Space(10)]
-        [SerializeField] private bool regenerateList;
-        public List<GameState> GameStates => gameStates;
-
-        private void OnValidate() {
-            if (regenerateList) {
-                GenerateList();
-                regenerateList = false;
-            }
-        }
-
-        public void GenerateList() {
-            
-            gameStates.Clear();
-            
-            string idString = "PER";
-                switch (statePrefix) {
-                    case StatePrefix.PER:
-                        idString = "PER";
-                        break;
-                    case StatePrefix.SC1:
-                        idString = "SC1";
-                        break;
-                    case StatePrefix.SC2:
-                        idString = "SC2";
-                        break;
-                    case StatePrefix.SC3:
-                        idString = "SC3";
-                        break;
-                }
-
-            foreach (StateType type in Enum.GetValues(typeof(StateType))) {
-
-                if (type.ToString().StartsWith(idString)) {
-                
-                    gameStates.Add(new GameState(type, resetStatesOnUnload));
-                }
-
-            }
-        }
-    }
-
-#endregion
     
     [Serializable]
     public class GameState {
