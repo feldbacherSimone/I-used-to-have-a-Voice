f54c35458c407cc1ebd2030b990b493406c8bd11
feldbacherSimone <simone.feldbacher@gmail.com>
2024-06-19 00:31:38 +0200

    add movemnt to main menu

---
+++ Assets/_IUTHAV/Scripts/UI/CameraTarget.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraTarget : MonoBehaviour
+{
+    [Range(0, 10)]
+    public float strength = 10;
+    public float yOffset = 0;
+    public float xOffset = 0;
+    public float zDepth = -2;
+    Vector3 initPos;
+    public bool camera; 
+    
+    private InputActions playerInputAction; 
+    private InputAction mouseMovement; 
+    private InputAction mouseScroll; 
+    
+    [SerializeField] float minDepth = -10;
+    [SerializeField] float maxDepth = 4;
+    private void Start()
+    {
+        initPos = gameObject.transform.position; 
+        playerInputAction = new InputActions();
+        playerInputAction.BaseActionMap.Enable();
+        mouseMovement = playerInputAction.BaseActionMap.MousePosition;
+        mouseScroll = playerInputAction.BaseActionMap.MouseScroll;
+        //mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>(); 
+    }
+    void moveCameraTarget()
+    {
+        // print(Input.mousePosition);
+        
+        float mouseX = mouseMovement.ReadValue<Vector2>().x ;
+        float mouseY = mouseMovement.ReadValue<Vector2>().y ;
+        float realitveMouseX = mouseX / Screen.width  - 0.5f + xOffset;
+        float realitveMouseY = mouseY / Screen.height - 0.5f + yOffset;
+
+        if (camera)
+        {
+            
+            if (zDepth >= minDepth && zDepth <= maxDepth)
+            {
+                zDepth += mouseScroll.ReadValue<Vector2>().y/1000;
+                //print("mouse Scroll: " + Input.mouseScrollDelta.y);
+            }
+            else if (zDepth < minDepth)
+            {
+                zDepth = minDepth;
+            }
+            else if (zDepth > maxDepth)
+            {
+                zDepth = maxDepth;
+            }
+        }
+        Vector3 newPos = new Vector3(initPos.x + realitveMouseX * strength, initPos.y + realitveMouseY * strength, initPos.z);
+        
+        gameObject.transform.position = newPos;
+        
+        // gameObject.transform.position = new Vector3(pos.x, pos.y, -2);
+
+    }
+
+    private void Update()
+    {
+        moveCameraTarget();
+    }
+}
