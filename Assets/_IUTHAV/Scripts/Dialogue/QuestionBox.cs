c053a1774f52a22cc3845f576d91e6a09990ac42
Martin Laptop <mart.siedler@gmail.com>
2024-06-26 13:12:36 +0200

    Bugfix Questionboxes

--- Assets/_IUTHAV/Scripts/Dialogue/QuestionBox.cs
+++ Assets/_IUTHAV/Scripts/Dialogue/QuestionBox.cs
@@ -17,17 +17,17 @@ namespace _IUTHAV.Scripts.Dialogue {
 
             _boxRectTransform = rectTransform;
 
-            if (_canvasGroup == null && gameObject.transform.childCount > 0) {
-                _canvasGroup = gameObject.GetComponent<CanvasGroup>();
+            if (BoxCanvasGroup == null) {
+                BoxCanvasGroup = gameObject.transform.GetChild(1).gameObject.GetComponent<CanvasGroup>();
             }
 
-            if (_canvasGroup != null) {
+            if (BoxCanvasGroup != null) {
             
-                _canvasGroup.alpha = 0;
+                BoxCanvasGroup.alpha = 0;
                 questionCanvasGroup.alpha = 0;
                 
-                _canvasGroup.blocksRaycasts = false;
-                _canvasGroup.interactable = false;
+                BoxCanvasGroup.blocksRaycasts = false;
+                BoxCanvasGroup.interactable = false;
                 questionCanvasGroup.blocksRaycasts = false;
                 questionCanvasGroup.interactable = false;
 
@@ -35,39 +35,43 @@ namespace _IUTHAV.Scripts.Dialogue {
 
                 bubble.transform.localScale = Vector3.zero;
 
+                question.gameObject.SetActive(false);
+
             }
             
             if (characterName.Equals("")) AutoAssignName();
         }
 
-        public void EnableQuestions() {
+        public void EnableQuestions(bool enable = true) {
             
-            StartCoroutine(FadeBox(true, questionCanvasGroup));
+            if (enable) Question.gameObject.SetActive(true);
+            StartCoroutine(FadeBox(enable, questionCanvasGroup));
         }
         
         protected override IEnumerator FadeBox(bool enable, CanvasGroup group) {
-        
-            //Create an artificial waittime, so it feels more natural+
+
             group.interactable = enable;
             group.blocksRaycasts = enable;
-
+            
             yield return new WaitForSeconds(Random.Range(0, maxRandomWaitTime));
-
+            
             float t = 0;
-
+            
             while (t < fadeTime) {
-
+                
                 float x = (enable) ? Mathf.Lerp(0.1f, 1, t / fadeTime) : Mathf.Lerp(1, 0.1f, t / fadeTime);
-
+                
                 group.alpha = x;
                 
-                if (group == _canvasGroup) bubble.transform.localScale = Vector3.Scale(initialScale, new Vector3(x, x, x));
+                if (group == BoxCanvasGroup) bubble.transform.localScale = Vector3.Scale(initialScale, new Vector3(x, x, x));
                 
                 t += Time.deltaTime;
                 yield return null;
             }
 
             if (!enable) group.alpha = 0;
+            
+            if (group == questionCanvasGroup && !enable) Question.gameObject.SetActive(false);
 
         }
         
