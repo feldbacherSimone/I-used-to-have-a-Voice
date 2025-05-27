83e711b6afb3a4c7858677586448e2b1c0e6d68e
Martin Laptop <mart.siedler@gmail.com>
2025-05-27 20:05:40 +0200

    Locked Act 2 to Dev Mode access only

---
+++ Assets/_IUTHAV/Scripts/Utility/DebugEventTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using _IUTHAV.Scripts.Core.Input;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace _IUTHAV.Scripts.Utility {
+    public class DebugEventTrigger : MonoBehaviour {
+        
+        private void OnEnable() {
+            InputController.OnDebug += ToggleDebugMenu;
+        }
+        
+        private void ToggleDebugMenu(InputAction.CallbackContext context) {
+            
+            GameObject debugMenu = transform.GetChild(0)?.gameObject;
+                
+            debugMenu.SetActive(!debugMenu.activeSelf);
+        }
+
+        private void OnDisable() {
+            InputController.OnDebug -= ToggleDebugMenu;
+        }
+    }
+}
